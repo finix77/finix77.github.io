901702533a6cf7204ed5e631ae85fdb3689b29fc
FinixUni <salvofinistrella@unimore.it>
2025-05-23 15:50:08 +0200

    extracted common pages

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,5 +1,9 @@
 // Basic smooth scrolling
 $(document).ready(function(){
+    $('#navbar-placeholder').load('assets/includes/navbar.html');
+    $('#header-placeholder').load('assets/includes/header.html');
+    $('#footer-placeholder').load('assets/includes/footer.html');
+
     $("a").on('click', function(event) {
         if (this.hash !== "") {
             event.preventDefault();
